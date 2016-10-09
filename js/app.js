a3c3abdb12d213f600af3d4c81924e2997e7c29f
Marc <phyrenight@gmail.com>
2016-10-08 20:35:11 -0400

    changed ip geo lookup to freegeoip.net

--- js/app.js
+++ js/app.js
@@ -1,14 +1,16 @@
 var tempMetric = 'C';
 var temp = ""
 
-var getWeather = function(lng,lat){
-	getLocation()
-    var $temp = $("#temp");
-    var $weather = $("#weather");
-    var $location = $("#location");
+var getWeather = function(locaCoords){//lng,lat){
+  var locationCoords = [];
+	//locationCoords = getLocation().then(function());
+  console.log(locaCoords);
+  var $temp = $("#temp");
+  var $weather = $("#weather");
+  var $location = $("#location");
 	var openWeatherUrl = "http://api.openweathermap.org/data/2.5/weather";
 	var openWeatherApiId = "f5b9a4274f04681ec6cb1740b22733a9";
-	var coords = "?lat="+lat+"&lon="+lng;
+	var coords = "?lat="+locaCoords.lat+"&lon="+locaCoords.lng;
 	openWeatherUrl = openWeatherUrl + coords+"&APPID=" + openWeatherApiId;
 	$temp.append(openWeatherUrl)
 	$.getJSON( openWeatherUrl, function(data) {
@@ -19,14 +21,15 @@ var getWeather = function(lng,lat){
 		var weather = data.weather[0].main;
 		var iconUrl = "http://openweathermap.org/img/w/" + icon;
 		var iconLink = "<img src='"+ iconUrl + ".png'alt='"+weather+"' title='"+weather+"'>"
-	    temp = kelvinToCelsius(data.main.temp);
-	    addBackground(weather);
-	    $weather.empty().append(iconLink);
-	    $temp.empty().append(temp+ " "+tempMetric);
-	    $location.empty().append(location);
-	    console.log(city+" , "+country);
+	  temp = kelvinToCelsius(data.main.temp);
+	  addBackground(weather);
+	  $weather.empty().append(iconLink);
+	  $temp.empty().append(temp+ " "+tempMetric);
+	  $location.empty().append(location);
+	  console.log(city+" , "+country);
 	});
 }
+
 var addBackground = function(condition){
   var $body = $("body");
   var imgURL = "";
@@ -80,27 +83,45 @@ var changeMetric = function(){
 var $x = $("#location");
 
 function getLocation() {
-  if (navigator.geolocation) {
-    location();
+  var lat = 0;
+  console.log("What the Hell!")
+  var lon = 0;
+  var IPURL = "http://freegeoip.net/json/";
+  $.getJSON(IPURL, function(result){
+    console.log(result.longitude)
+    console.log("llllll")
+    var locaCoords = {
+    lng:result.longitude,
+    lat: result.latitude
+    }
+  getWeather(locaCoords);
+  });
+ // getWeather(lat,lon);
+}
+
+
+ /* if (navigator.geolocation) {
+    locations();
   } else {
    $x.empty().append("Geolocation is not supported by browser.");
   }
-}
-function location(){
+}*/
+/*function locations(){
   var options = {
     enableHighAccuracy: false,
     timeout: 5000,
     maximumAge: 0
   };
   navigator.geolocation.getCurrentPosition(success, error, options)
-}
+}*/
 
 function success(pos){
-  var lng = pos.coords.longitude;
-  var lat = pos.coords.latitude;
+  var lng = pos.lon;
+  var lat = pos.lat;
   getWeather(lng,lat)
+  console.log(lng)
 }
-
+/*
 function error(err){
   $x.empty().append("something went wrong "+ err.code);
-}
+}*/
